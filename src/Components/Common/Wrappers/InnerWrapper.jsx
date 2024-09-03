bfc3db17ed049ff122bc82c73393738017f2c285
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-09-03 15:00:27 +0200

    issue #6 variabler, mixins og global styling oprettet

--- src/Components/Common/Wrappers/InnerWrapper.jsx
+++ src/Components/Common/Wrappers/InnerWrapper.jsx
@@ -1,7 +1,6 @@
-import style from "./InnerWrapper.module.scss";
+import globalStyle from "../../../Styles/GlobalStyles.module.scss";
 
 // Bruges til at lave en "boks" hvor indholdet er indeni
-
 export const InnerWrapper = ({ children }) => {
-	return <section className={style.InnerWrapper}>{children}</section>;
+	return <section className={globalStyle.InnerWrapper}>{children}</section>;
 };
